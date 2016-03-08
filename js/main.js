244d4c292e13f2a7cb013713396b96d6f17400a6
lucifer63 <lazalu68@gmail.com>
2016-03-09 00:54:09 +0300

    Slider is now works in IE

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var $body = $('body'), $window = $('window'), $eyes = $( '.eye' ), eye_active;
+var $body = $('body'), 
+	$window = $('window'), 
+	$eyes = $( '.eye' ), 
+	eye_active = -1;
 
 function sign(x) {
     return typeof x === 'number' ? x ? x < 0 ? -1 : 1 : x === x ? 0 : NaN : NaN;
@@ -24,15 +27,15 @@ $( '.modal-overlay, .modal-close' ).on('click', function() {
 	$body.removeClass('modal-active');
 });
 
-$eyes.each(function(i) { $( this ).attr('data-id', i); });
+$eyes.each(function(i) { $( this ).attr('data-index', i); });
 $eyes.on('click', function() {
-	var id = $( this ).attr('data-id');
+	var id = $( this ).attr('data-index');
 
-	if (eye_active) {
+	if (eye_active !== -1) {
 		$eyes.eq( eye_active ).removeClass('active');
 	}
 
-	if (id == eye_active) {
+	if (id === eye_active) {
 		eye_active = -1;
 	} else {
 		eye_active = id;
@@ -40,23 +43,52 @@ $eyes.on('click', function() {
 	}
 });
 
+jQuery.support.cssTransitions = (function() {
+    var b = document.body || document.documentElement,
+        s = b.style,
+        p = 'transition';
+
+    if (typeof s[p] == 'string') { return true; }
+
+    // Tests for vendor specific prop
+    var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
+    p = p.charAt(0).toUpperCase() + p.substr(1);
+
+    for (var i=0; i<v.length; i++) {
+        if (typeof s[v[i] + p] == 'string') { return true; }
+    }
+
+    return false;
+})();
+
+
 $.fn.createSlider = function (opt) {
     var options = $.extend(true, {
     	touch_control: true,
-    	repeat_slides: true
+    	repeat_slides: true,
+    	change_thresold: 10,
+    	control_overlay_selector: 'slider-control-overlay'
     }, opt);
 
-    console.log(options);
+    // change_thresold - minimal distance in percentage of the slider width that user should move slide at to change it
+
+	if (typeof options.change_thresold !== 'number' && options.change_thresold < 0 || options.change_thresold > 100) {
+		console.log('Invalid \'change_thresold\' value: it is \'' + options.change_thresold + '\', but expected to be a number greater than 0 and lesser than 100. Slider can be created with default thresold value - 10% !');
+		options.change_thresold = 10;
+	}
 
     return this.each(function () {
     	var base = $( this ), 
     		list = base.find( options.list_selector ), 
+    		control_overlay = base.find( options.control_overlay_selector ),
     		arrows = base.find( options.arrows_selector ), 
     		slides = base.find( options.slides_selector ),
     		map_items = base.find( options.map_items_selector ),
-    		slides_arr, current, get, changeTo, 
-    		width = base.width(), drag_start_position, relative_position,
-    		touch_x, flick;
+    		width = base.width(), 
+    		thresold = options.change_thresold,
+    		slides_arr, current,
+    		drag_start_position, relative_position, touch_x, flick, 
+    		get, changeTo, animate;
 
     	if (!list.length) {
     		console.log('Slider can\'t be created: list element wasn\'t found in the specified slider container: ', base);
@@ -71,47 +103,14 @@ $.fn.createSlider = function (opt) {
     		return; 
     	}
 
-    	slides_arr = slides.toArray();
-    	current = 0;
-    	get = options.repeat_slides ? function(i) {
-    		if ( i<0 ) {
-    			return get( i += slides_arr.length );
-    		} else if (i >= slides_arr.length ) {
-    			return get( i -= slides_arr.length );
-    		}
-    		return i;
-    	} : function(i) {
-    		if ( i === slides_arr.length || i === -1 ) {
-    			return current;
-    		}
-    		return i;
-    	};
-		changeTo = options.repeat_slides ? function(target) {
-			current = get( target );
-			list.css('right', current*100 + '%');
-			map_items.removeClass('active').eq(current).addClass('active');
-		} : function(target) {
-			if (target === 0) {
-				arrows.eq(0).addClass('inactive');
-			} else if (target === slides_arr.length - 1) {
-				arrows.eq(1).addClass('inactive');
-			}
-			if (current === 0 && target > current) {
-				arrows.eq(0).removeClass('inactive');
-			} else if (current === slides_arr.length -1 && target < current) {
-				arrows.eq(1).removeClass('inactive');
-			}
-			
-			current = get( target );
-			map_items.removeClass('active').eq(current).addClass('active');
-			list.css('right', current*100 + '%');
-		};
+    	if (!control_overlay.length) {
+    		control_overlay = $('<div class="' + options.control_overlay_selector + '"></div>');
+    		base.prepend( control_overlay );
+    	}
 
-	    /*$( 'body' ).on('keydown',function(e) {
-	      if (e.keyCode == 39 || e.keyCode == 37) {
-	        changeTo(current+(e.keyCode-38));
-	      }
-	    })*/
+    	current = 0;
+    	slides_arr = slides.toArray();
+
 	    if ( map_items.length ) {
 	    	if ( map_items.length !== slides_arr.length ) {
 	    		console('Warning: the amount of map items is not equal to amount of slides in the specified slider container: ', base);
@@ -122,6 +121,10 @@ $.fn.createSlider = function (opt) {
 	    		});
 	    	});
 	    }
+
+	    $( window ).on('resize', function() {
+	    	width = base.width();
+	    });
 
 	    arrows.each(function() {
 			var direction;
@@ -146,31 +149,88 @@ $.fn.createSlider = function (opt) {
 			}
 	    });
 
-	    $( window ).on('resize', function() {
-	    	width = base.width();
-	    });
+    	get = options.repeat_slides ? 
+	    	function(i) {
+	    		if ( i<0 ) {
+	    			return get( i += slides_arr.length );
+	    		} else if (i >= slides_arr.length ) {
+	    			return get( i -= slides_arr.length );
+	    		}
+	    		return i;
+	    	} : 
+	    	function(i) {
+	    		if ( i === slides_arr.length || i === -1 ) {
+	    			return current;
+	    		}
+	    		return i;
+	    	};
 
-	    base.on({
-	    	'mousedown': function(e) {
+		changeTo = options.repeat_slides ? 
+			function(target) {
+				current = get( target );
+				animate();
+				map_items.removeClass('active').eq(current).addClass('active');
+			} : 
+			function(target) {
+				if (target === 0) {
+					arrows.eq(0).addClass('inactive');
+				} else if (target === slides_arr.length - 1) {
+					arrows.eq(1).addClass('inactive');
+				}
+				if (current === 0 && target > current) {
+					arrows.eq(0).removeClass('inactive');
+				} else if (current === slides_arr.length -1 && target < current) {
+					arrows.eq(1).removeClass('inactive');
+				}
+				
+				current = get( target );
+				map_items.removeClass('active').eq(current).addClass('active');
+				animate();
+			};
+
+		animate = jQuery.support.cssTransitions ? 
+			function() {
+				list.css('right', current*100 + '%');
+			} :
+			function() {
+				list.stop(true, false).animate({ 'right': current*100 + '%' }, 300);
+			};
+
+	    /*$( 'body' ).on('keydown',function(e) {
+	      if (e.keyCode == 39 || e.keyCode == 37) {
+	        changeTo(current+(e.keyCode-38));
+	      }
+	    })*/
+
+	    control_overlay.on({
+	    	'mousedown': jQuery.support.cssTransitions ?
+			function(e) {
 	    		base.addClass('dragged');
 		    	drag_start_position = e.offsetX;
+		    	return false;
+		    } :
+		    function(e) {
+	    		base.addClass('dragged');
+		    	drag_start_position = e.offsetX;
+		    	list.stop(true, false);
 		    	return false;
 		    },
 		    'mousemove': function(e) {
 		    	if (drag_start_position) {
 		    		// if first or last - restrict moving past them
-		    		if ((current === 0 && drag_start_position - e.offsetX < 0) || (current === slides_arr.length - 1 && drag_start_position - e.offsetX > 0)) {
+		    		if ((current === 0 && drag_start_position - e.offsetX < 0) ||
+		    			(current === slides_arr.length - 1 && drag_start_position - e.offsetX > 0)) {
 		    			return;
 		    		}
 		    		relative_position = (drag_start_position - e.offsetX) * (100/width);
-			    	list.css('right', current*100 + relative_position + '%');
+		    		list.css('right', current*100 + relative_position + '%');
 			    }
 		    },
 		    'mouseup': function(e) {
-		    	if ( Math.abs(relative_position) > 25 ) {
+		    	if ( Math.abs(relative_position) > thresold ) {
 		    		changeTo( current + sign(relative_position) );
 		    	} else {
-		    		list.css('right', current*100 + '%');
+		    		animate();
 		    	}
 		    	drag_start_position = 0;
 		    	relative_position = 0;
@@ -178,8 +238,10 @@ $.fn.createSlider = function (opt) {
 		    }, 
 		    'mouseleave': function(e) {
 		    	if (drag_start_position) {
-			    	if ( Math.abs(relative_position) > 25 ) {
+			    	if ( Math.abs(relative_position) > thresold ) {
 			    		changeTo( current + sign(relative_position) );
+			    	} else {
+			    		animate();
 			    	}
 			    	drag_start_position = 0;
 			    	relative_position = 0;
@@ -187,6 +249,8 @@ $.fn.createSlider = function (opt) {
 			    }
 		    }
 	    });
+
+
 	    if (options.touch_control) {
 
 			/*if (navigator.msMaxTouchPoints) {
@@ -200,18 +264,22 @@ $.fn.createSlider = function (opt) {
 			}*/
 
 			base.on({
-				'touchstart': function(event) {
+				'touchstart': function(e) {
 					flick = true;
-					drag_start_position = event.originalEvent.touches[0].pageX;
+					drag_start_position = e.originalEvent.touches[0].pageX;
+
+					if (jQuery.support.cssTransitions) {
+						list.stop(true, false);
+					}
 
 					setTimeout(function() {
 						flick = false;
 					}, 250);
 					
 					base.addClass('dragged');
-				},
-				'touchmove': function(event) {
-					touch_x = event.originalEvent.touches[0].pageX;
+				}, 
+				'touchmove': function(e) {
+					touch_x = e.originalEvent.touches[0].pageX;
 
 		    		if ((current === 0 && drag_start_position - touch_x < 0) || 
 		    			(current === slides_arr.length - 1 && drag_start_position - touch_x > 0)) {
@@ -221,13 +289,27 @@ $.fn.createSlider = function (opt) {
 		    		relative_position = (drag_start_position - touch_x) * (100/width);
 			    	list.css('right', current*100 + relative_position + '%');
 				},
-				'touchend': function(event) {
-					if ( Math.abs(relative_position) > 25 || flick ) {
+				'touchend': function() {
+					if ( Math.abs(relative_position) > thresold || flick ) {
 			    		changeTo( current + sign(relative_position) );
+			    	} else {
+			    		animate();
 			    	}
 			    	drag_start_position = 0;
 			    	relative_position = 0;
 			    	base.removeClass('dragged');
+				},
+				'touchcancel': function() {
+			    	if (drag_start_position) {
+				    	if ( Math.abs(relative_position) > thresold || flick ) {
+				    		changeTo( current + sign(relative_position) );
+				    	} else {
+				    		animate();
+				    	}
+				    	drag_start_position = 0;
+				    	relative_position = 0;
+				    	base.removeClass('dragged');
+				    }
 				}
 			});
 	    } // touch control END
